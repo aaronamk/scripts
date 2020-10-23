d45f21ef76c0d03eeb96477f8bf0baf8f4c64d3a
aaronamk <amklein@email.wm.edu>
2020-10-23 18:20:16 -0400

    consistency and POSIX compliance.

--- brightness-ctrl.sh
+++ brightness-ctrl.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+case $1 in
+	"+") brightnessctl set 70+ ;;
+	"-") brightnessctl set 69\- ;;
+esac
 
+if [ -n "$(xrandr --listactivemonitors | grep "DP1 2560")" ]; then
+	brightness=$(brightnessctl -d intel_backlight g)
+	sudo ddcutil --bus=5 setvcp 10 $(( $brightness / 10 - 8 ))
+fi
